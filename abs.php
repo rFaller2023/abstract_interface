62d0437a4213a2b2746f97372ec03b61de54426d
rFaller2023 <rfaller989@gmail.com>
2023-05-13 13:19:57 +0800

    first commit

--- abs.php
+++ abs.php
@@ -11,6 +11,9 @@
     public function _construct()
     {
         $this->conn=new mysqli ($this->servername, $this->username, $this->password);
+        $db=("CREATE DATABASE IF NOT EXISTS $this->dbname");
+        // var_dump($this->conn->error);
+        $this->conn->query(db);
     }
-    abstract public function db(): string;
+    // abstract public function db(): string;
  }
