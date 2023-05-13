5cbdc4ee243eca9df76210736b4c8c36d12722a2
rFaller2023 <rfaller989@gmail.com>
2023-05-13 14:06:04 +0800

    DATA

--- abs.php
+++ abs.php
@@ -12,9 +12,19 @@
     {
         $this->conn=new mysqli ($this->servername, $this->username, $this->password);
     }
-    abstract public function db(): string; {
-        public $conn;
-        return "Database";
-    }
 
- }
+
+    
+    abstract public function dbname() : string; 
+        
+}
+class Abstract extends Database
+{
+    public function dbname() : string;
+    {
+        $data="CREATE DATABASE IF NOT EXISTS $this->dbname";
+        return $this->conn->query($data);
+    }
+}
+$rose= new Database();
+$rose->dbname();
